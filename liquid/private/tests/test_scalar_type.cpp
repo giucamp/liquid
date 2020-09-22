7191fda53f410dbba752ec6f93764dcb7a924d44
giuseppe.campana <giu.campana@gmail.com>
2020-09-22 06:03:35 +0200

    fix - added missing file

---
+++ liquid/private/tests/test_scalar_type.cpp
@@ -0,0 +1,26 @@
+
+//   Copyright Giuseppe Campana (giu.campana@gmail.com) 2020.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include "liquid.h"
+#include <iostream>
+
+namespace liquid
+{
+    void TestScalarType()
+    {
+        std::cout << "TestScalarTypefd...";
+
+        LIQUID_ASSERT(GetScalarType<Real>() == ScalarType::Real);
+        LIQUID_ASSERT(GetScalarType<Integer>() == ScalarType::Integer);
+        LIQUID_ASSERT(GetScalarType<Bool>() == ScalarType::Bool);
+
+        static_assert(std::is_same_v<FromScalarType<ScalarType::Real>, Real>);
+        static_assert(std::is_same_v<FromScalarType<ScalarType::Integer>, Integer>);
+        static_assert(std::is_same_v<FromScalarType<ScalarType::Bool>, Bool>);
+
+        std::cout << "done" << std::endl;
+    }
+}
